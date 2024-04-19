1c176e3951e1bcc33070f059f412b10895542baa
simonsba955 <simonsba955@northwested.org>
2024-04-19 13:26:24 -0400

    Updated main.js

--- main.js
+++ main.js
@@ -6,9 +6,18 @@ const num2 = parseInt(prompt('Enter second number:\n'));
 
 //PROCESS
 const addition = num1 + num2;
-const subraction = num1 - num2;
+const subtraction = num1 - num2;
 const multiplication = num1 * num2;
 const divison = num1 / num2;
 
 //OUTPUT
-console.log('Output;', addition, subraction, multiplication, divison);
+console.log('Bailey Simons');
+console.log('19 April 2024');
+console.log('JS Simple Calculator');
+
+console.log('Output:')
+
+console.log(num1 + "+" + num2 + "=" + addition);
+console.log(num1 + "-" + num2 + "=" + subtraction);
+console.log(num1 + "x" + num2 + "=" + multiplication);
+console.log(num1 + "/" + num2 + "=" + divison);
