6ad7d88c89d71aab1bf64d5c7c821cdb7f071271
simonsba955 <simonsba955@northwested.org>
2024-04-19 12:58:53 -0400

    Created project files

---
+++ main.js
@@ -0,0 +1,14 @@
+// Bailey Simons, 4/19/24, JS Simple Calculator
+
+// INPUT
+const num1 = parseInt(prompt('Enter first number:\n'));
+const num2 = parseInt(prompt('Enter second number:\n'));
+
+//PROCESS
+const addition = num1 + num2;
+const subraction = num1 - num2;
+const multiplication = num1 * num2;
+const divison = num1 / num2;
+
+//OUTPUT
+console.log('Output;', addition, subraction, multiplication, divison);
